fea608c68ebdf2895a604cb6ad5e2460b55e6272
Joseph Angeletti <jangelet@gmail.com>
2018-02-25 17:44:26 -0500

    Function to delete head node of a singly linked list.
    Function to delete head node of a singly linked list.

---
+++ deleteAtHead.java
@@ -0,0 +1,11 @@
+// java.util.* and java.util.streams.* have been imported for this problem.
+// You don't need any other imports.
+
+public ListNode deleteAtHead(ListNode head) {
+    if(head != null){
+        head = head.next;
+        return head;
+    }else{
+        return null;
+    }
+}

e099b19f4ad72337529592cb38903a3068fa6b3e
Joseph Angeletti <jangelet@gmail.com>
2018-03-28 23:43:55 -0400

    Uses buffer array to determine if two input strings are anagrams of each other (linear time and constant space)

---
+++ anagramsLinear.java
@@ -0,0 +1,19 @@
+public static boolean isAnagram(String input1, String input2) {
+    
+    if(input1 == null || input2 == null || input1.length() != input2.length()) return false;
+    
+    int[] buffer = new int[26];
+    
+    for(int i = 0; i < input1.length(); i++){
+        System.out.println(input1.charAt(i) - 'a');
+        System.out.println(input2.charAt(i) - 'a');
+        buffer[input1.charAt(i) - 'a']++;
+        buffer[input2.charAt(i) - 'a']--;
+    }
+    
+    for(int i = 0; i < buffer.length; i++){
+        if(buffer[i] != 0) return false;
+    }
+    
+    return true;
+    

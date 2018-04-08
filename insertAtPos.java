b87f39211ff6148fa1a5c46a474aa00ab0b61b1d
Joseph Angeletti <jangelet@gmail.com>
2018-04-07 23:00:27 -0400

    Inserts node at target position in doubly linked list

--- insertAtPos.java
+++ insertAtPos.java
@@ -3,41 +3,44 @@
 
 public DoublyLinkedNode insertAtPos(DoublyLinkedNode head, int data, int pos) {
     
-    //cycle through LL using two tracker variables, increment curr_pos, when curr_pos == pos
-        //create new node with data, have prev pointer next point new node, curr node prev point to new node
+    DoublyLinkedNode new_node = new DoublyLinkedNode(data);
+    
+    if(head == null && pos > 1) return null;
+    
+    if(head == null && pos == 1) return new_node;
     
     int curr_pos = 1;
-    DoublyLinkedNode n_node = new DoublyLinkedNode(data);
     
-    if(head == null && pos > 1) return head;
+    DoublyLinkedNode temp = head;
     
-    if(head == null && pos == 1) return n_node;
-    
+    //if target position is head and LL isn't empty
     if(pos == 1){
-        head = n_node;
+        new_node.next = temp;
+        temp.prev = new_node;
+        return new_node;
+    }
+    
+    //traverses list until curr_pos equals target position, or tail is reached
+    while(curr_pos != pos && temp.next != null){
+        temp = temp.next;
+        curr_pos++;
+    }
+    
+    //if target position is immediately after tail
+    if(curr_pos + 1 == pos){
+        temp.next = new_node;
+        new_node.prev = temp;
+        return head;
+    //if target position is between head and tail
+    }else if(curr_pos == pos){
+        new_node.next = temp;
+        new_node.prev = temp.prev;
+        temp.prev.next = new_node;
+        temp.prev = new_node;
+        return head;
+    //if target position is outside bounds
+    }else{
         return head;
     }
-    
-    DoublyLinkedNode curr = head;
-    
-    while(pos != curr_pos){
-        curr_pos++;
-        DoublyLinkedNode prev = curr;
-        if(curr.next != null) curr = curr.next;
-        if(pos == curr_pos){
-            // if(curr != head){
-            //     n_node.next = curr;
-            // }else if(curr.next = null){
-            //     n_node.next = null;
-            // }
-            n_node.next = curr;
-            n_node.prev = prev;
-            prev.next = n_node;
-            if(curr.prev != null) curr.prev = n_node;
-        }
-        
-    }
-    
-    return head; 
-    
+
 }
